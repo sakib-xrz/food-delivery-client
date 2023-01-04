5bf89bca2a210807b3702d9623bf028ea8f81b6e
sakib-xrz <sakibxrz21@gmail.com>
2023-01-04 17:32:45 +0600

    Navbar responsive almost done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "./Navbar.css";
 import Logo from "../../assets/images/logo.png";
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { VscThreeBars } from "react-icons/vsc";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav>
       <div className="nav">
@@ -11,7 +15,7 @@ const Navbar = () => {
           <img src={Logo} alt="" />
         </Link>
         <div className="menu-items">
-          <ul className="nav-links">
+          <ul className={isOpen ? "nav-links-mobile" : "nav-links"}>
             <NavLink to={"/home"}>
               <li>Home</li>
             </NavLink>
@@ -21,11 +25,22 @@ const Navbar = () => {
             <NavLink to={"/orders"}>
               <li>My Orders</li>
             </NavLink>
+            <button className="login">Log In</button>
           </ul>
-          <button className="login">
-            Log In
-          </button>
         </div>
+        <button className="mobile-menu">
+          {isOpen ? (
+            <VscChromeClose
+              onClick={() => setIsOpen(false)}
+              className="bar-close"
+            />
+          ) : (
+            <VscThreeBars
+              onClick={() => setIsOpen(true)}
+              className="bar-open"
+            />
+          )}
+        </button>
       </div>
     </nav>
   );
