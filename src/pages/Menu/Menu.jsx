ca8700d45c05aa980492ce5ddebbda0ef3aee8d1
sakib-xrz <sakibxrz21@gmail.com>
2023-01-05 17:56:14 +0600

    some bug fix

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import Card from "../../components/Card/Card";
-import LargeSpinner from '../../components/Spinner/LargeSpinner/LargeSpinner';
-import SmallSpinner from '../../components/Spinner/SmallSpinner/SmallSpinner';
 import "./Menu.css"
 
 const Menu = () => {
         return (
           <div className="container">
-            <h1>This is Home page</h1>
+            <h1 className='home-title'>Don’t starve, just order</h1>
             <div className="card-div">
               <Card />
               <Card />
               <Card />
               <Card />
             </div>
-            <SmallSpinner />
-            <LargeSpinner />
           </div>
         );
 };
