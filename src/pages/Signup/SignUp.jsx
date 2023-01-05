bfa9815bdaf8f7c82f68aa8fd7268ba2ff77b4d3
sakib-xrz <sakibxrz21@gmail.com>
2023-01-05 15:29:44 +0600

    get value from signup field

--- src/pages/Signup/SignUp.jsx
+++ src/pages/Signup/SignUp.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import "./SignUp.css";
 import { AiOutlineUser } from "react-icons/ai";
 import { SlEnvolope } from "react-icons/sl";
 import { SlLock } from "react-icons/sl";
@@ -8,64 +9,134 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-      const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [error, setError] = useState(false);
 
-    return (
-      <div className="box">
-        <div className="form-container log-in-container">
-          <form>
-            <h1>Sign Up</h1>
-            <small className="toggle-page">
-              Already have an account?
-              <Link to={"/login"} className="toggle-button">
-                {" "}
-                Log In
-              </Link>
-            </small>
-            <div className="form-div">
-              <div className="single-input">
-                <span>
-                  <AiOutlineUser className="icon" />
-                </span>
-                <input type="text" placeholder="Your name" />
-              </div>
-              <div className="single-input">
-                <span>
-                  <SlEnvolope className="icon" />
-                </span>
-                <input type="email" placeholder="Email address" />
-              </div>
-              <div className="single-input">
-                <span>
-                  <SlLock className="icon" />
-                </span>
-                {isVisible ? (
-                  <input type="text" placeholder="Password" />
-                ) : (
-                  <input type="password" placeholder="Password" />
-                )}
-                {!isVisible ? (
-                  <span>
-                    <AiOutlineEye
-                      onClick={() => setIsVisible(true)}
-                      className="show-hide"
-                    />
-                  </span>
-                ) : (
-                  <span>
-                    <AiOutlineEyeInvisible
-                      onClick={() => setIsVisible(false)}
-                      className="show-hide"
-                    />
-                  </span>
-                )}
-              </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+  };
+
+  const handlePassword = (e) => {
+    if (!/(?=.*[A-Z])/.test(e.target.value)) {
+      setError("Password should contain at least one upper case");
+      return;
+    } else {
+      setError("");
+    }
+    if (!/(?=.*[a-z])/.test(e.target.value)) {
+      setError("Password should contain at least one lower case");
+      return;
+    } else {
+      setError("");
+    }
+    if (!/(?=.*?[0-9])/.test(e.target.value)) {
+      setError("Password should contain at least one digit");
+      return;
+    } else {
+      setError("");
+    }
+    if (!/(?=.*?[!@#$&*~])/.test(e.target.value)) {
+      setError("Password should contain at least one Special character");
+      return;
+    } else {
+      setError("");
+    }
+    if (!/.{8,}/.test(e.target.value)) {
+      setError("Password Must be at least 8 characters in length");
+      return;
+    } else {
+      setError("");
+    }
+    setError("");
+  };
+
+  return (
+    <div className="box">
+      <div className="form-container log-in-container">
+        <form onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
+          <small className="toggle-page">
+            Already have an account?
+            <Link to={"/login"} className="toggle-button">
+              {" "}
+              Log In
+            </Link>
+          </small>
+          <div className="form-div">
+            <div className="single-input">
+              <span>
+                <AiOutlineUser className="icon" />
+              </span>
+              <input type="text" name="name" placeholder="Your name" required />
             </div>
-            <button className="login-btn">Sign Up</button>
-          </form>
-        </div>
+            <div className="single-input">
+              <span>
+                <SlEnvolope className="icon" />
+              </span>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email address"
+                required
+              />
+            </div>
+            <div className="single-input">
+              <span>
+                <SlLock className="icon" />
+              </span>
+              {isVisible ? (
+                <input
+                  onChange={handlePassword}
+                  type="text"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              ) : (
+                <input
+                  onChange={handlePassword}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              )}
+              {!isVisible ? (
+                <span>
+                  <AiOutlineEye
+                    onClick={() => setIsVisible(true)}
+                    className="show-hide"
+                  />
+                </span>
+              ) : (
+                <span>
+                  <AiOutlineEyeInvisible
+                    onClick={() => setIsVisible(false)}
+                    className="show-hide"
+                  />
+                </span>
+              )}
+            </div>
+          </div>
+          <small className="error">{error}</small>
+          {error ? (
+            <button type="submit" className="login-btn-disabled" disabled>
+              Sign Up
+            </button>
+          ) : (
+            <button type="submit" className="login-btn">
+              Sign Up
+            </button>
+          )}
+        </form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SignUp;
